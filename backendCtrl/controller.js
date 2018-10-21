8f324db773408513ce38a7958a1f012b2aba9a6d
Cherry Rose Semena <cph-cs241@cphbusiness.dk>
2018-10-21 20:19:41 +0200

    cleaning up

--- backendCtrl/controller.js
+++ backendCtrl/controller.js
@@ -80,94 +80,10 @@ ctrl.delete = async function(id) {
 ctrl.getStories = async function(){
   try {
     let res = await axios.get(`${backendUrl}item/all`)
-    // for (var idx in res.data.items){
-    //   console.log(`this data is ${JSON.stringify(res.data.items[idx], null, 2)} `)
-    // }
     return res.data
 
   } catch (e) {
     console.log(`getStories ERROR: ${e}`)
-  }
-}
-
-
-//-----------------------------PLAYGROUND----------------------------------------------------
-let util = require('../_temp/util')
-
-ctrl.getHomeContent = async function() {
-  try {
-    let res = await axios.get(backendUrl)
-    for (var idx in res.data) {
-      console.log(`The data is ${JSON.stringify(res.data[idx], null, 2)}`)
-    }
-    return res.data
-  } catch (e) {
-    console.log(`getHomeContent ERROR: ${e.toString()}`)
-    return {
-      'statusCode': 500,
-      'errorMessage': e.toString()
-    }
-  }
-}
-
-ctrl.logintest = async function(user) {
-  try {
-    let users = require('../_temp/tempusers.json')
-    let res = {}
-    var userDb = _.find(users, {
-      'username': user.username
-    });
-    if (userDb != null) {
-      if (userDb.username === user.username && userDb.password === user.password) {
-        res.statusCode = 200
-        res.user = userDb
-      } else {
-        res.statusCode = 400
-        res.errorMessage = 'Bad login.'
-      }
-    } else {
-      res.statusCode = 400
-      res.errorMessage = 'UNKNOWN USER'
-    }
-    return res
-  } catch (e) {
-    console.log(`loginTest ERROR ${e.toString()}`)
-    return {
-      'statusCode': 500,
-      'errorMessage': e.toString()
-    }
-  }
-}
-
-ctrl.registertest = async function(user) {
-  try {
-    util.saveUser(user)
-    return {
-      'statusCode': 200,
-      'user': user
-    }
-  } catch (e) {
-    console.log(`resgisterTest ERROR: ${e.toString()}`)
-    return {
-      'statusCode': 500,
-      'errorMessage': e.toString()
-    }
-  }
-}
-
-ctrl.submittest = async function(story) {
-  try {
-    util.saveStory(story)
-    return {
-      'statusCode': 200,
-      'story': story
-    }
-  } catch (e) {
-    console.log(`resgisterTest ERROR: ${e.toString()}`)
-    return {
-      'statusCode': 500,
-      'errorMessage': e.toString()
-    }
   }
 }
 
